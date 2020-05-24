cda75b27f8b2ff342408369eb1017d9783d2470a
velmor99 <volgagaz24100@gmail.com>
2020-05-25 02:40:06 +0300

    first done

---
+++ task-03.js
@@ -0,0 +1,33 @@
+const randomIntegerFromInterval = (min, max) => {
+	return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const makeTransaction = (transaction) => {
+	return new Promise((resolve, reject) => {
+		const delay = randomIntegerFromInterval(200, 500);
+		setTimeout(() => {
+			const canProcess = Math.random() > 0.3;
+			if (canProcess) {
+				resolve(delay); //resolve принимает только 1 параметр
+			} else {
+				reject(transaction.id);
+			}
+		}, delay);
+	});
+};
+
+const logSuccess = (id) => {
+	console.log(`Transaction ${id} done`);
+}; //Немного переделано в связи с тем что resolve принимает только 1 параметр
+
+const logError = (id) => {
+	console.warn(`Error processing transaction ${id}. Please try again later.`);
+};
+
+makeTransaction({ id: 70, amount: 150 }).then(logSuccess).catch(logError);
+
+makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError);
+
+makeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError);
+
+makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError);
